bded39b95807e55aeb0f9e7358a30a48d95f9393
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-11-03 23:06:38 +0100

    Add UserDaysEditor component and new function to save object on App state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Calendar from 'react-calendar'
+import UserDaysEditor from './UserDaysEditor';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,36 +10,10 @@ class App extends React.Component {
     }
   }
 
-  handleChangeCalendar = (date) => {
-    const inputUserDate = document.querySelector("#inputUserDate");
-    inputUserDate.value = date.toLocaleDateString();
-  }
-
-  handleGetUserData = () => {
-    const inputUserDate = document.querySelector("#inputUserDate");
-    const selectedDate = inputUserDate.value;
-    const sadRadio = document.getElementById("sad");
-    const happyRadio = document.getElementById("happy");
-    const textArea = document.getElementById("text-area");
-    const textAreaValue = textArea.value;
-    console.log(textAreaValue);
-    let checkedRadio;
-    if(sadRadio.checked) {
-      checkedRadio = sadRadio.value
-    }
-    else {
-      checkedRadio = happyRadio.value
-    }
-    console.log(checkedRadio);
-    const userData = {
-      date: selectedDate,
-      status: checkedRadio,
-      message: textAreaValue,
-    };
-    console.log('soy el objeto', userData);
+  saveUserDay = (userDataObject) => {
     const userDayDataArray = this.state.userData;
     userDayDataArray.push(
-      userData
+      userDataObject
     )
     this.setState({
       userData: userDayDataArray
@@ -54,32 +28,10 @@ class App extends React.Component {
           <h2 className="header__subtitle">Elige la fecha y un smiley para añadir tu estado de ánimo</h2>
         </header>
         <main className="app__main-wrapper">
-          <section className="section__user-editor">
-            <form id="form" className="user-editor__form">
-            <Calendar 
-              onChange={this.handleChangeCalendar}
-            />
-            <input required id="inputUserDate" type="text" name="selected-date"/>
-              <div className="user-editor__radio">
-                <p className="user-editor__text">Elige tu estado</p>
-                <label htmlFor="happy">Happy</label>
-                <input required id="happy" type="radio" name="status" value="happy"/>
-                <label htmlFor="sad">Sad</label>
-                <input required id="sad" type="radio" name="status" value="sad"/>
-              </div>
-              <div className="user-editor__textarea">
-                <p className="user-editor__text">Escribe por qué tu día fue tan bueno</p>
-                <textarea id="text-area" rows="4" cols="50">
-                </textarea>
-              </div>
-              <div className="user-editor__buttons">
-                <button onClick={this.handleGetUserData} type="submit" form="form1" value="Submit">Guardar</button>
-                <button type="submit" form="form1" value="Submit">Cancelar</button>
-              </div>
-            </form>
-
-          </section>
-          <section className="section__user-days"></section>
+          <UserDaysEditor
+            saveUserDay = {this.saveUserDay}
+          />
+          <section className="section__user-days-history"></section>
         </main>
       </div>
     );
---
+++ src/UserDaysEditor.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Calendar from 'react-calendar'
+
+class UserDaysEditor extends React.Component {
+  handleChangeCalendar = (date) => {
+    const inputUserDate = document.querySelector("#inputUserDate");
+    inputUserDate.value = date.toLocaleDateString();
+  }
+
+  handleGetUserData = () => {
+    const inputUserDate = document.querySelector("#inputUserDate");
+    const selectedDate = inputUserDate.value;
+    const sadRadio = document.getElementById("sad");
+    const happyRadio = document.getElementById("happy");
+    const textArea = document.getElementById("text-area");
+    const textAreaValue = textArea.value;
+    console.log(textAreaValue);
+    let checkedRadio;
+    if(sadRadio.checked) {
+      checkedRadio = sadRadio.value
+    }
+    else {
+      checkedRadio = happyRadio.value
+    }
+    console.log(checkedRadio);
+    const userData = {
+      date: selectedDate,
+      status: checkedRadio,
+      message: textAreaValue,
+    };
+    console.log('soy el objeto', userData);
+    this.props.saveUserDay(userData);
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <section className="section__user-editor">
+            <form id="form" className="user-editor__form">
+            <Calendar 
+              onChange={this.handleChangeCalendar}
+            />
+            <input required id="inputUserDate" type="text" name="selected-date"/>
+              <div className="user-editor__radio">
+                <p className="user-editor__text">Elige tu estado</p>
+                <label htmlFor="happy">Happy</label>
+                <input required id="happy" type="radio" name="status" value="happy"/>
+                <label htmlFor="sad">Sad</label>
+                <input required id="sad" type="radio" name="status" value="sad"/>
+              </div>
+              <div className="user-editor__textarea">
+                <p className="user-editor__text">Escribe por qué tu día fue tan bueno</p>
+                <textarea id="text-area" rows="4" cols="50">
+                </textarea>
+              </div>
+              <div className="user-editor__buttons">
+                <button onClick={this.handleGetUserData} type="submit" form="form1" value="Submit">Guardar</button>
+                <button type="submit" form="form1" value="Submit">Cancelar</button>
+              </div>
+            </form>
+          </section>
+      </React.Fragment>
+    )
+  }
+}
+
+export default UserDaysEditor;
