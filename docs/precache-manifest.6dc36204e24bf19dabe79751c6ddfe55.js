5958fb0117259e35753a4ad729534f16b07015ab
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-11-04 18:09:49 +0100

    Run build and docs

---
+++ docs/precache-manifest.6dc36204e24bf19dabe79751c6ddfe55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f81f89d56c4b1bfb2888284f3b6bdf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1ead8ea44607805152ac",
+    "url": "./static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "bbab8ed4cdece7f93920",
+    "url": "./static/css/main.cc4b8e32.chunk.css"
+  },
+  {
+    "revision": "1ead8ea44607805152ac",
+    "url": "./static/js/2.354c83cc.chunk.js"
+  },
+  {
+    "revision": "bbab8ed4cdece7f93920",
+    "url": "./static/js/main.f4a90be8.chunk.js"
+  },
+  {
+    "revision": "cc27230fac8d8ea3d576",
+    "url": "./static/js/runtime-main.314c9d34.js"
+  }
+]);
